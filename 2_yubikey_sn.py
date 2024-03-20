336cb039d06958a6b077078b3a8450a63a440efe
UncleMonty <31314488+UncleMonty@users.noreply.github.com>
2024-03-20 18:48:02 -0500

    Delete 2_yubikey_sn.py

--- 2_yubikey_sn.py
+++
@@ -1,8 +0,0 @@
-from ykman.device import list_all_devices
-from yubikit.core.smartcard import SmartCardConnection
-import time
-
-for device, info in list_all_devices():
-    if info.version >= (5, 0, 0):
-        print(f"YubiKey Serial Number Detected: {info.serial}")
-    time.sleep(30) 
